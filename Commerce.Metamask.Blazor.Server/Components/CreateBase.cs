e6a62081d22a397b892e5fe557eba9ff3c14bbac
KevinSmall <kevin_small@hotmail.com>
2019-11-26 22:17:01 +0000

    Commerce wallet UI - PO create works with metamask

--- Commerce.Metamask.Blazor.Server/Components/CreateBase.cs
+++ Commerce.Metamask.Blazor.Server/Components/CreateBase.cs
@@ -7,6 +7,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Commerce.Metamask.Blazor.Server.Models;
+using Commerce.Metamask.Blazor.Server.Services;
+using Commerce.Buyer.Lib.Models;
 
 namespace Commerce.Metamask.Blazor.Server.Components
 {
@@ -23,8 +25,8 @@ namespace Commerce.Metamask.Blazor.Server.Components
         public string AdditionalMessage { get; set; }
         public bool IsBusy { get; private set; }
 
-        //[Inject]
-        //public HttpClient Client { get; set; }
+        [Inject]
+        public IWalletBuyer WalletBuyer { get; set; }
 
         protected override void OnInitialized()
         {
@@ -32,7 +34,7 @@ namespace Commerce.Metamask.Blazor.Server.Components
             {
                 BuyerPurchaseOrderNumber = BuyerPoNumber,
                 SellerSysId = "SoylentCorporation",
-                BuyerProductId = "BHT-1101 this needs updated",
+                BuyerProductId = "BHT-1101",
                 Currency = "DAI",
                 CurrencyAddress = "0x7bbd0d72e59ede7f2a65abde9539aa006682c741",
                 TotalQuantity = 4,
@@ -49,40 +51,20 @@ namespace Commerce.Metamask.Blazor.Server.Components
 
             try
             {
-                //var web3 = new Web3(new Account(Settings.AccountWithEtherKey), Settings.BlockchainUrl);
-                //var wbs = new WalletBuyerService(web3, Settings.WalletBuyerAddress);
-
-                //// Check PO number is new
-                //var poMainAddress = await wbs.PoMainQueryAsync();
-                //var poms = new PoMainService(web3, poMainAddress);
-                //var buyerSysId = await wbs.SystemIdQueryAsync();
-                //var buyerSysIdAsString = ConversionUtils.ConvertBytes32ArrayToString(buyerSysId);
-                //var buyerPoNumberAsBytes = ConversionUtils.ConvertStringToBytes32Array(BuyerPoNumber);
-                //var po = await poms.GetPoByBuyerPoNumberQueryAsync(buyerSysId, buyerPoNumberAsBytes);
-
-                //if (po != null && po.Po != null && po.Po.EthPurchaseOrderNumber != 0)
-                //{
-                //    // PO already exists    
-                //    AdditionalMessage = $"Buyer PO {BuyerPoNumber} already exists as Eth PO {po.Po.EthPurchaseOrderNumber}";
-                //}
-                //else
-                //{
-                //    // Create new PO
-                //    PoCreateModel.BuyerPurchaseOrderNumber = BuyerPoNumber; // refresh model value from parent value
-                //    Po newPo = CreatePoDefn
-                //    (
-                //        buyerSysId: buyerSysIdAsString,
-                //        buyerPurchaseOrderNumber: PoCreateModel.BuyerPurchaseOrderNumber,
-                //        sellerSysId: PoCreateModel.SellerSysId,
-                //        buyerProductId: PoCreateModel.BuyerProductId,
-                //        currency: PoCreateModel.Currency,
-                //        currencyAddress: PoCreateModel.CurrencyAddress,
-                //        totalQuantity: PoCreateModel.TotalQuantity,
-                //        totalValue: PoCreateModel.TotalValue
-                //    );
-                //    AdditionalMessage = $"Creation request sent for Buyer PO {PoCreateModel.BuyerPurchaseOrderNumber}";
-                //    ReceiptForCreateRequest = await wbs.CreatePurchaseOrderRequestAndWaitForReceiptAsync(newPo);
-                //}
+                // Check PO number is new
+                var po = await WalletBuyer.Lib.PurchaseOrder.GetPoAsync(BuyerPoNumber).ConfigureAwait(false);
+                if (po != null && po.EthPurchaseOrderNumber != 0)
+                {
+                    // PO already exists    
+                    AdditionalMessage = $"Buyer PO {BuyerPoNumber} already exists as Eth PO {po.EthPurchaseOrderNumber}";
+                }
+                else
+                {
+                    // Create new PO
+                    PoCreateModel.BuyerPurchaseOrderNumber = BuyerPoNumber; // refresh model value from parent value
+                    AdditionalMessage = $"Creation request being sent for Buyer PO {PoCreateModel.BuyerPurchaseOrderNumber}";
+                    ReceiptForCreateRequest = await WalletBuyer.Lib.PurchaseOrder.CreatePoAndWaitForReceiptAsync(PoCreateModel).ConfigureAwait(false);
+                }
             }
             catch (Exception ex)
             {
@@ -93,37 +75,5 @@ namespace Commerce.Metamask.Blazor.Server.Components
                 IsBusy = false;
             }
         }
-
-        //private Po CreatePoDefn(
-        //    string buyerSysId,
-        //    string buyerPurchaseOrderNumber,
-        //    string sellerSysId,
-        //    string buyerProductId,
-        //    string currency,
-        //    string currencyAddress,
-        //    uint totalQuantity,
-        //    uint totalValue)
-        //{
-        //    return new Po()
-        //    {
-        //        //EthPurchaseOrderNumber;                               // leave initial, filled by PoMain
-        //        BuyerSysId = buyerSysId,
-        //        BuyerPurchaseOrderNumber = buyerPurchaseOrderNumber,
-        //        BuyerViewVendorId = string.Empty,                       // leave initial, filled by PoMain
-        //        SellerSysId = sellerSysId,
-        //        SellerSalesOrderNumber = string.Empty,                  // leave initial, filled by PoMain
-        //        SellerViewCustomerId = string.Empty,                    // leave initial, filled by PoMain
-        //        BuyerProductId = buyerProductId,
-        //        SellerProductId = string.Empty,                         // leave initial, filled by PoMain
-        //        Currency = currency,
-        //        CurrencyAddress = currencyAddress,
-        //        TotalQuantity = totalQuantity,
-        //        TotalValue = totalValue,
-        //        //OpenInvoiceQuantity; leave initial, filled by PoMain
-        //        //OpenInvoiceValue; leave initial, filled by PoMain
-        //        //PoStatus; leave initial, filled by PoMain
-        //        //WiProcessStatus; leave initial, filled by PoMain
-        //    };
-        //}
     }
 }
